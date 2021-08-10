8fb35caa2f1790dea81828c798fe977b5d581f84
shanyongsy <shanyong@shanyong-pc.localdomain>
2021-08-10 19:49:28 +0800

    fix:修正log。

--- server/server.go
+++ server/server.go
@@ -12,7 +12,7 @@ package main
 import (
 	"bufio"
 	"flag"
-	"fmt"
+	"log"
 	"net"
 	"os"
 	"strconv"
@@ -21,61 +21,65 @@ import (
 
 var addr = flag.String("addr", "", "The address to listen to; default is \"\" (all interfaces).")
 var port = flag.Int("port", 8000, "The port to listen on; default is 8000.")
+var clientIndex int = 0
 
 func main() {
 	flag.Parse()
 
-	fmt.Println("Starting server...")
+	log.Println("Starting server...")
 
 	src := *addr + ":" + strconv.Itoa(*port)
 	listener, _ := net.Listen("tcp", src)
-	fmt.Printf("Listening on %s.\n", src)
+	log.Printf("Listening on %s.\n", src)
 
 	defer listener.Close()
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Printf("Some connection error: %s\n", err)
+			log.Printf("Some connection error: %s\n", err)
+			continue
 		}
 
+		clientIndex++
+		log.Printf("server info : client index is %v.", clientIndex)
 		go handleConnection(conn)
 	}
 }
 
 func handleConnection(conn net.Conn) {
 	remoteAddr := conn.RemoteAddr().String()
-	fmt.Println("Client connected from " + remoteAddr)
+	log.Println("Client connected from " + remoteAddr)
 
 	scanner := bufio.NewScanner(conn)
 
 	for {
 		ok := scanner.Scan()
 
+		handleMessage(scanner.Text(), conn)
+
 		if !ok {
 			break
 		}
-
-		handleMessage(scanner.Text(), conn)
 	}
 
-	fmt.Println("Client at " + remoteAddr + " disconnected.")
+	log.Println("Client at " + remoteAddr + " disconnected.")
 }
 
 func handleMessage(message string, conn net.Conn) {
-	fmt.Println("> " + message)
+	log.Println(conn.RemoteAddr().String() + "> " + message)
 
 	if len(message) > 0 && message[0] == '/' {
 		switch {
 		case message == "/time":
 			resp := "It is " + time.Now().String() + "\n"
-			fmt.Print("< " + resp)
+			log.Print(conn.RemoteAddr().String() + "< " + resp)
 			conn.Write([]byte(resp))
 
 		case message == "/quit":
-			fmt.Println("Quitting.")
+			log.Println("Quitting.")
 			conn.Write([]byte("I'm shutting down now.\n"))
-			fmt.Println("< " + "%quit%")
+			log.Println(conn.RemoteAddr().String() + "< " + "%quit%")
 			conn.Write([]byte("%quit%\n"))
 			os.Exit(0)
 
